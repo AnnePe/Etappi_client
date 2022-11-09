f3df3480859402e0275f19e11c0c59433a83e2af
Taina <kankainen.taina@gmail.com>
2022-11-09 20:49:37 +0200

    lippujen haku, koodin siistimistä

--- src/components/Lipunmyynti.js
+++ src/components/Lipunmyynti.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Alert } from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Lipunmyynti () {
 
@@ -7,68 +7,57 @@ function Lipunmyynti () {
   const [lippu, setLippu] = useState('');
   const [linkki,setLinkki] = useState('');
   const [lippulinkki,setLippulinkki] = useState('');
-  const [tapahtumat, setTapahtumat] = useState('');
+  const [tapahtumat, setTapahtumat] = useState({});
 
   // const timestamp = Date.now(); // This would be the timestamp you want to format
-
   // const time= (new Intl.DateTimeFormat('en-US', {year: 'numeric', month: '2-digit',day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit'}).format(timestamp));
+
   const haeMyynti = () => {
-     
+    console.log(id);
     fetch(`https://etappi-ticketguru.herokuapp.com/api/myyntitapahtumat/${id}`)
     .then(response => response.json()
     .then(responseJson => setLippu(responseJson))
     .catch(error => { 
-      console.log('Error', error); 
+      console.error(error)
     }));
-    console.log(lippu);
+    console.log(lippu)
     setStatus('onnistui get')
     setLinkki(lippu._links.self.href);
     setLippulinkki(lippu._links.liput.href);
   }
-  
+
+  useEffect(() => {
+    haeMyynti();
+    haeMyyntiliput();
+  }, [id]);
+
   const haeMyyntiliput = () => {
-     
+    //fetch(`https://etappi-ticketguru.herokuapp.com/api/myyntitapahtumat/${id}/liput`)
     fetch(`${lippulinkki}`)
     .then(response => response.json()
     .then(responseJson => setTapahtumat(responseJson))
     .catch(error => { 
-      console.log('Error', error); 
+      console.error(error)
     }));
     console.log(tapahtumat);
     setStatus('myyntitapahtuman liput haettu')
-    console.log(tapahtumat._embedded.lippus);
-   // setLinkki(lippu._links.self.href);
-  //  setLippulinkki(lippu._links.liput.href);
   }
 
-
-  const Lipuntiedot = ({ lippu }) => 
-  <p>
-    {lippu.lippukoodi} {lippu.hinta}€ {lippu._links.self.href}
-  </p>
-  
-  const Liput = () => {
-    return (
-      Object.values(tapahtumat._embedded.lippus).map(lippu =>
-        <Lipuntiedot 
-        key={lippu.id}
-        lippu={lippu}
-        />)
-    )
-  }
-/*
-  const Liput = () => {
-    return (
-      <div>tekstiä</div>
-    )
-  }*/
-
-  const vaihdaId = (event) => {
-    event.preventDefault();
-    haeMyynti(id);
+  const Liput = ({ tapahtumat }) => {
+    if (tapahtumat) {
+      return (
+        Object.values(tapahtumat._embedded.lippus).map(lippu =>
+          <p key={lippu.id}>
+            {lippu.lippukoodi} {lippu.hinta}€ {lippu._links.self.href}</p>
+          ))
+    } else {
+      return (
+        <div> tyhjä </div>
+      ) 
+    }
   }
 
- const lisaaLippu = () => {
+  const lisaaLippu = () => {
   //https://etappi-ticketguru.herokuapp.com/api/myyntitapahtumat/1/liput
   //https://etappi-ticketguru.herokuapp.com/api/liput/
   // "myyntitapahtuma": "https://etappi-ticketguru.herokuapp.com/api/myyntitapahtumat/3" 
@@ -81,76 +70,62 @@ function Lipunmyynti () {
         "Content-Type": "application/json"
       },
     }).then(function(response) {
-  
     setStatus('Lippu lisätty haetulle myyntitapahtumalle');
       return response.text()
     }, function(error) {
-  //    error.message //=> String
+      console.error(error)
     setStatus('erhe');
     })
   }
+
   const luoMyyntitapahtuma = () => {
-    //https://etappi-ticketguru.herokuapp.com/api/myyntitapahtumat/
-    //{    "kayttaja": "http://localhost:8080/api/kayttajat/1" }
-        fetch(`https://etappi-ticketguru.herokuapp.com/api/myyntitapahtumat/`, { 
-        method: "POST",
-       body: JSON.stringify({"kayttaja":"http://localhost:8080/api/kayttajat/1"}),
-        headers: {
-          "Content-Type": "application/json"
-        },
-      }).then(function(response) {
-    
+    fetch(`https://etappi-ticketguru.herokuapp.com/api/myyntitapahtumat/`, { 
+      method: "POST",
+      body: JSON.stringify({"kayttaja":"http://localhost:8080/api/kayttajat/1"}),
+      headers: {
+        "Content-Type": "application/json"
+      },
+    }).then(function(response) {
       setStatus('Myyntitapahtuma lisätty');
-        return response.text()
+      return response.text()
       }, function(error) {
-    //    error.message //=> String
+        console.error(error)
       setStatus('erhe');
-      })
-    }
+    })
+  }
 
-  /*
-
-  */
+  const handleChange = (event) => {
+    event.preventDefault();
+    haeMyynti(id);
+  }
 
   return (
     <div>
-
-      <form onSubmit={vaihdaId}>
-        <label>
-          Anna myyntitapahtuma id(paina 2 kertaa että päivittyy näytölle): 
-          <input
-            type="text"
-            value={id}
-            onChange={(e) => setId(e.target.value)}
-          />
-        </label>
-        <input type="submit" value="Hae" />
-        
-      </form>
-
-      <p>
-     myyntitapahtuma url {linkki}<br />
-     lippu url {lippulinkki}<br />
-        Toteutunut {lippu.toteutunut}<br />
-       tähän vois tuoda allekain tehdyt liput haeMyyntiliput<br /><br />
-       
-       
-       
-
-        <button onClick={lisaaLippu}>
-          Lisää lippu
-        </button>
-        <button onClick={luoMyyntitapahtuma}>
-          Luo myyntitapahtuma
-        </button>
-        <button onClick={haeMyyntiliput}>
-          Näytä myyntitapahtuman liput
-        </button>
-        <Liput />
-      </p>
-      <p> 
-        <div>{status}</div>
-    </p>
+      <div>
+        <label>Anna myyntitapahtuma id: </label>
+      <input 
+        value={id}
+        onChange={e => setId(e.target.value)}/>
+      <button onClick={handleChange}>Hae</button>
+      </div>
+      <div>
+        <p>
+          myyntitapahtuma url {linkki}<br />
+          lippu url {lippulinkki}<br />
+          Toteutunut {lippu.toteutunut}<br />
+          <br />
+          <button onClick={lisaaLippu}>
+            Lisää lippu
+          </button>
+          <button onClick={luoMyyntitapahtuma}>
+            Luo myyntitapahtuma
+          </button>
+          <button onClick={haeMyyntiliput}>
+            Näytä myyntitapahtuman liput
+          </button>
+        </p>
+      </div>
+      <Liput tapahtumat={tapahtumat}/>
     </div>
   );
 }
